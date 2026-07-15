a38fa7074d7131b8fdf407296eba6fc4a4ed8b80
danwiz <danewisdom@gmail.com>
2026-07-15 17:28:06 -0400

    refactor(identity): store explicit account role

--- src/com/revature/cardealer/User.java
+++ src/com/revature/cardealer/User.java
@@ -1,27 +1,32 @@
 package com.revature.cardealer;
 
-
-
 public class User {
-	
-	private String username;
-	
-	private String password;
-	
-	public String getUsername() {
-		return this.username;
-	}
-	
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	
-	public String getPassword() {
-		return this.password;
-	}
-	
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
+
+    private String username;
+    private String password;
+    private AccountRole role = AccountRole.CUSTOMER;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public AccountRole getRole() {
+        return role;
+    }
+
+    public void setRole(AccountRole role) {
+        this.role = role;
+    }
 }
