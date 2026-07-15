91eca6e8222bf777c7f79f792f2145080c13385e
danwiz <danewisdom@gmail.com>
2026-07-15 18:07:49 -0400

    refactor: make account state serializable

--- src/com/revature/cardealer/User.java
+++ src/com/revature/cardealer/User.java
@@ -1,6 +1,10 @@
 package com.revature.cardealer;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private String username;
     private String password;
