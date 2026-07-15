d2b70302364429644e7585712cc05dfb9fc267f9
danwiz <danewisdom@gmail.com>
2026-07-15 17:27:35 -0400

    feat(authz): add explicit account roles

---
+++ src/com/revature/cardealer/AccountRole.java
@@ -0,0 +1,10 @@
+package com.revature.cardealer;
+
+/**
+ * Explicit application roles assigned to registered accounts.
+ */
+public enum AccountRole {
+    CUSTOMER,
+    EMPLOYEE,
+    ADMINISTRATOR
+}
