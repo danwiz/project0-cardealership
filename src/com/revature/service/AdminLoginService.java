2bd9165d7654e4903d4d27c1524bacb3a22cd092
danwiz <danewisdom@gmail.com>
2026-07-15 17:18:05 -0400

    refactor: allow administrator service repository injection

--- src/com/revature/service/AdminLoginService.java
+++ src/com/revature/service/AdminLoginService.java
@@ -1,19 +1,27 @@
 package com.revature.service;
 
-import com.revature.cardealer.*;
 import com.revature.DAOService.DAOService;
 import com.revature.DAOService.DataDAO;
+import com.revature.cardealer.User;
 
 public class AdminLoginService extends UserLoginService {
 
-	DataDAO cDao = new DAOService();
+    DataDAO cDao = new DAOService();
 
-	@Override
-	public boolean authenticateUser(User user) {
-		return super.authenticateUser(user);
-	}
+    public AdminLoginService() {
+        super();
+    }
 
-	public void deleteAllUsers() {
-		System.out.println("You better be sure you want to do this!!!!!!!");
-	}
+    public AdminLoginService(UserAccountRepository userRepository) {
+        super(userRepository);
+    }
+
+    @Override
+    public boolean authenticateUser(User user) {
+        return super.authenticateUser(user);
+    }
+
+    public void deleteAllUsers() {
+        System.out.println("You better be sure you want to do this!!!!!!!");
+    }
 }
