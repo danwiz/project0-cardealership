ad8d03574c7f048b236c9d24f0c03ac54dffcbd7
danwiz <danewisdom@gmail.com>
2026-07-15 17:38:00 -0400

    fix: use domain account role in console tests

--- src/test/java/com/revature/cardealer/CarDealershipCharacterizationTest.java
+++ src/test/java/com/revature/cardealer/CarDealershipCharacterizationTest.java
@@ -18,8 +18,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import com.revature.service.AccountRole;
-
 class CarDealershipCharacterizationTest {
 
     private final PrintStream originalOut = System.out;
