bdc62bcdbc45d6d9ecb18cb45d51a17f3cf82b66
danwiz <danewisdom@gmail.com>
2026-07-16 00:37:02 -0400

    refactor: define atomic payment processor port

---
+++ src/com/revature/application/PaymentProcessor.java
@@ -0,0 +1,8 @@
+package com.revature.application;
+
+import com.revature.cardealer.PaymentTransaction;
+
+/** Atomic boundary for applying a payment to one customer ownership plan. */
+public interface PaymentProcessor {
+    PaymentTransaction record(String customerName, int ownershipIndex, int amount);
+}
