276d59a7e73e1f825f6a293b2987b2db6d28a505
Dane Wisdom <danewisdom@gmail.com>
2026-07-19 07:18:55 -0400

    refactor: accept acquisition contracts in ownership repositories

--- src/com/revature/repository/OwnershipRepository.java
+++ src/com/revature/repository/OwnershipRepository.java
@@ -8,9 +8,16 @@ import com.revature.cardealer.OwnedVehicle;
 
 public interface OwnershipRepository {
     List<OwnedVehicle> findAll();
+
     default Optional<OwnedVehicle> findById(long ownershipId) {
         return findAll().stream().filter(vehicle -> vehicle.getOwnershipId() == ownershipId).findFirst();
     }
+
     void add(Car car, int purchasePrice, int paymentMonths);
+
+    default void add(String contractId, Car car, int purchasePrice, int paymentMonths) {
+        add(car, purchasePrice, paymentMonths);
+    }
+
     void replaceAll(List<OwnedVehicle> vehicles);
 }
