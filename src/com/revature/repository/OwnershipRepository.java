7060d24daee2a2082ecf3735212c0743ec3f6b48
Dane Wisdom <danewisdom@gmail.com>
2026-07-18 23:57:49 -0400

    refactor: address ownership records by stable id

--- src/com/revature/repository/OwnershipRepository.java
+++ src/com/revature/repository/OwnershipRepository.java
@@ -1,12 +1,14 @@
 package com.revature.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.revature.cardealer.Car;
 import com.revature.cardealer.OwnedVehicle;
 
 public interface OwnershipRepository {
     List<OwnedVehicle> findAll();
-    void add(Car car, int purchasePrice, int paymentMonths);
+    Optional<OwnedVehicle> findById(long ownershipId);
+    long add(Car car, int purchasePrice, int paymentMonths);
     void replaceAll(List<OwnedVehicle> vehicles);
 }
